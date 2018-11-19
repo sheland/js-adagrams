02d3f6c5d351dc2f39a738e39380b2fecba1c9a3
Shelan <shelandebesai@gmail.com>
2018-11-18 18:11:10 -0800

    created usesAvailableLetters method

--- src/adagrams.js
+++ src/adagrams.js
@@ -28,31 +28,46 @@ const Adagrams = {
     "Z": 1,
   },
 
-randomKeys() {
-  let result = []
-  for (let i = 0; i < 10; i++) {
-    let keys = Object.keys(Adagrams.letters);
-    let curr = keys[Math.floor(Math.random()*keys.length)];
-      result.push(curr);
-      Adagrams.letters[curr] -= 1;
-      if (Adagrams.letters[curr] < 1) {
-        console.log('this is current:',curr);
-        delete Adagrams.letters[curr]
+  randomKeys() {
+    const result = []
+    for (let i = 0; i < 10; i++) {
+      const keys = Object.keys(Adagrams.letters);
+      const curr = keys[Math.floor(Math.random()*keys.length)];
+        result.push(curr);
+        Adagrams.letters[curr] -= 1;
+        if (Adagrams.letters[curr] < 1) {
+          delete Adagrams.letters[curr]
+        }
+    }
+    console.log('this is all that is left in the hash of letters:',Adagrams.letters);
+
+      return result;
+    }
+  }
+
+// console.log(Adagrams.randomKeys());
+
+
+  usesAvailableLetters(input, letters_in_hand) {
+    const input = input.split('');
+    const letters_in_hand = Adagrams.randomKeys();
+    const tru_fals = [];
+
+    for (let input_each of input) {
+      if (letters_in_hand.includes(input_each)) {
+        tru_false.push(input_each)
+      } else {
+        tru_false.push("false");
       }
+    }
+    if (tru_fals.includes("false")) {
+      return false
+    } else {
+      return true
+    }
   }
-  console.log('this is all that is left in the hash of letters:',Adagrams.letters);
-
-    return result;
-  }
-}
 
 
-console.log(Adagrams.randomKeys());
-
-  // letters[letter] -= 1
-  // if letters[letter] < 0
-  // letters.delete(letter)
-  // if !letters[letter] console.log("not available");
   // drawLetters(letters) {
   //   let letters = `${"A".repeat(9) + "B".repeat(2) + "C".repeat(2) + "D".repeat(4) + "E".repeat(12) + "F".repeat(2) + "G".repeat(3) + "H".repeat(2) + "D".repeat(4) + "E".repeat(12) + "F".repeat(2) + "G".repeat(3) + "H".repeat(2) + "I".repeat(9) + "J".repeat(1) + "K".repeat(1) + "L".repeat(4) + "M".repeat(2) + "N".repeat(6) + "O".repeat(8) + "P".repeat(2) + "Q".repeat(1) + "R".repeat(6) + "S".repeat(4) + "T".repeat(6) + "U".repeat(4) + "V".repeat(2) + "W".repeat(2) + "Y".repeat(2) + "Z".repeat(1)`.split('');
 //
@@ -61,19 +76,6 @@ console.log(Adagrams.randomKeys());
 //   }
 // },
 
-
-//   usesAvailableLetters(input, letters_in_hand)
-//
-//   let input = input.split("")
-//
-//   let tru_fals = {}
-//
-//   input.forEach(element)
-//     if (letters_in_hand.includes(element))
-//       tru_fals << letters_in_hand.delete_at(letters_in_hand.index(let))
-
-//
-// };
 
 
 // Do not remove this line or your tests will break!
